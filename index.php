93d384829f258814168e75909dce7700de89a733
Rip-van-Vinkl <bookdiler@yandex.ru>
2022-01-19 21:50:48 +0300

    init

--- index.php
+++ index.php
@@ -1,53 +1,42 @@
 <?php
-$example_persons_array = [
-    [
-        'fullname' => 'Иванов Иван Иванович',
-        'job' => 'tester',
-    ],
-    [
-        'fullname' => 'Степанова Наталья Степановна',
-        'job' => 'frontend-developer',
-    ],
-    [
-        'fullname' => 'Пащенко Владимир Александрович',
-        'job' => 'analyst',
-    ],
-    [
-        'fullname' => 'Громов Александр Иванович',
-        'job' => 'fullstack-developer',
-    ],
-    [
-        'fullname' => 'Славин Семён Сергеевич',
-        'job' => 'analyst',
-    ],
-    [
-        'fullname' => 'Цой Владимир Антонович',
-        'job' => 'frontend-developer',
-    ],
-    [
-        'fullname' => 'Быстрая Юлия Сергеевна',
-        'job' => 'PR-manager',
-    ],
-    [
-        'fullname' => 'Шматко Антонина Сергеевна',
-        'job' => 'HR-manager',
-    ],
-    [
-        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
-        'job' => 'analyst',
-    ],
-    [
-        'fullname' => 'Бардо Жаклин Фёдоровна',
-        'job' => 'android-developer',
-    ],
-    [
-        'fullname' => 'Шварцнегер Арнольд Густавович',
-        'job' => 'babysitter',
-    ],
-];
-?>
+
+interface Go {public function go();}
+interface GoDown {public function goDown();}
+interface Action{public function action();}
+
+abstract class Technic implements Go, GoDown, Action
+{
+    public function go()
+    {echo 'еду вперёд';}
+
+    public function goDown()
+    {echo 'еду назад';}
+
+    public function action()
+    {echo 'что-то делаю';} 
+}
+
+class Car extends Technic
+{
+    public function goCar() {$this->go();}
+    public function actionCar() {$this->action();}
+}
 
 
-<?php
-include 'main.php';
-?>
+class Excavator extends Technic
+{
+    public function goCar() {$this->go();}
+    public function actionCar() {$this->action();}
+}
+
+
+
+$car = new Car();
+
+function testTechnic(Car $car) {
+    $car->goCar();
+    $car->actionCar();
+
+}
+
+testTechnic($car);
